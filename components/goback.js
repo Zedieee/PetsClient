5ab90448fe690d0235c765c46ae3eaa62155ccbb
BrianGC <gargallocarnallabrianhermam@gmail.com>
2022-12-20 21:16:54 -0600

    csschanges2

--- components/goback.js
+++ components/goback.js
@@ -1,6 +1,7 @@
 import {useRouter} from 'next/router'
 
 export default function goBack({name}) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const router = useRouter()
     const handelClick = (e) => {
         router.push({
